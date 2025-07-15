6ed254b3bc6e6cd7240ebc8f3874bc8a968256ec
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-07-15 17:00:28 +0700

    add assets all you need

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-red-600 font-bold'>Hello world</h1>
+      <h1 className='text-[100px] text-yellow-600 font-bold'>Hello world</h1>
     </>
   )
 }
